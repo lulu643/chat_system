1596a52c33d2ba4eada8318b40204d67f747ee0d
Lulu6964 <sl6964@nyu.edu>
2020-12-05 17:08:36 +0800

    Reverted back
    revert back to the orginal state

--- build_GUI/interface.py
+++ build_GUI/interface.py
@@ -9,29 +9,29 @@ def get_weather(city):
     weather = response.json()
     label['text'] = format_response(weather)
 
-def create_interface():
-    root = tk.Tk()
 
-    canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-    canvas.pack()
+root = tk.Tk()
 
-    background_image = tk.PhotoImage(file='images/2.png')
-    background_label = tk.Label(root, image=background_image)
-    background_label.place(relwidth=1, relheight=1)
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
 
-    frame = tk.Frame(root, bg='#80c1ff', bd=5)
-    frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+background_image = tk.PhotoImage(file='images/2.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
-    entry = tk.Entry(frame, font=40)
-    entry.place(relwidth=0.65, relheight=1)
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-    button = tk.Button(frame, text="SEND", font=40, command=lambda: get_weather(entry.get()))
-    button.place(relx=0.7, relheight=1, relwidth=0.3)
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
 
-    lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-    lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+button = tk.Button(frame, text="SEND", font=40, command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
 
-    label = tk.Label(lower_frame)
-    label.place(relwidth=1, relheight=1)
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-    root.mainloop()
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+
+root.mainloop()
